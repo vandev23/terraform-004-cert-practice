06ffb44029f4abc75fcbe1bb76c0095f8f7ee8f8
vandev23 <vane.pacheco@gmail.com>
2026-07-20 22:33:38 -0400

    feat: add lab 02 - create your first resource

---
+++ labs/lab_02_create_your_first_resource/providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = "1.12.2"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+  profile = "terraform-practice"
+}
